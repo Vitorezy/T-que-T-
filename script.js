421cca5f5d915cdad73bd200a8b04eec28c275c1
Vitor Gonçalves <vitorgondev@gmail.com>
2024-08-12 21:39:32 -0300

    V2

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
     var Navbar = document.querySelector("nav")
     var Header = document.querySelector("header")
     var main = document.querySelector('main')
-    Header.addEventListener('mouseover', () => { 
-        Header.style.height = "300px"
-        Navbar.style.height = "200px"
-        main.style.top = "300px"
+    Navbar.addEventListener('click', () => { 
+        Header.style.height = "30%"
+        Navbar.style.height = "70%"
+        main.style.top = "36.5%"
     })   
     Navbar.addEventListener('mouseleave', () => { 
-        Header.style.height = "100px"
+        Header.style.height = "11%"
         Navbar.style.height = "20px"
-        main.style.top = "100px"
+        main.style.top = "17.5%"
     })
     
     
