b8223989ace3e8c1d56727e72c819fc7a2c25ada
Vitor Gonçalves <vitorgondev@gmail.com>
2024-08-23 22:12:41 -0300

    melhorando a responsividade

--- script.js
+++ script.js
@@ -5,13 +5,18 @@
     var conteudo = document.getElementById("conteudo")
     var nav_aberta = document.getElementById('nav_aberta')
     Navbar.addEventListener('click', () => { 
-        if(nav_aberta.style.visibility ="hidden") { 
+
             nav_aberta.style.visibility = "visible"
-        }
-        else if (nav_aberta.style.visibility ="visible") { 
-            nav_aberta.style.visibility = "hidden"
-        }
+
     });  
+    Navbar.addEventListener('dblclick', () => { 
+
+        nav_aberta.style.visibility = "hidden"
+
+});  
+    main.addEventListener('click', ()=> { 
+        nav_aberta.style.visibility = "hidden"
+    })
     nav_aberta.addEventListener('mouseleave', () => { 
         
         nav_aberta.style.visibility = "hidden"
