2d4785031b39f375a5b6a17b364a3a1967df62e9
Vitor Gonçalves <vitorgondev@gmail.com>
2024-08-28 21:44:51 -0300

    evoluindo na estilização e animação das paginas

--- Script-js/script.js
+++ Script-js/script.js
@@ -1,25 +1,22 @@
     
+    
     var Navbar = document.querySelector("nav")
     var Header = document.querySelector("header")
     var main = document.querySelector('main')
     var conteudo = document.getElementById("conteudo")
     var nav_aberta = document.getElementById('nav_aberta')
     Navbar.addEventListener('click', () => { 
-
-            nav_aberta.style.visibility = "visible"
-
+            nav_aberta.style.height = "35%"
     });  
     Navbar.addEventListener('dblclick', () => { 
-
-        nav_aberta.style.visibility = "hidden"
-
+        nav_aberta.style.height = "0%"
 });  
     main.addEventListener('click', ()=> { 
-        nav_aberta.style.visibility = "hidden"
+        nav_aberta.style.height = "0%"
     })
     nav_aberta.addEventListener('mouseleave', () => { 
         
-        nav_aberta.style.visibility = "hidden"
+        nav_aberta.style.height = "0%"
     });
     
     
