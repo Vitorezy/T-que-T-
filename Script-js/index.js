d149c04e0a7dad92d6ffa0c60db0de676899e87c
Vitor Gonçalves <vitorgondev@gmail.com>
2024-09-06 17:25:07 -0300

    Criando checkbox do index

---
+++ Script-js/index.js
@@ -0,0 +1,8 @@
+var botaoTodos = document.getElementById('btnNavTodas')
+botaoTodos.addEventListener('click', ()=> { 
+    if(botaoTodos.checked) { 
+        console.log('funfou')
+    } else { 
+        console.log('desmarcado')
+    }
+})
