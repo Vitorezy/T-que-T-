d7c50b639dba1af22b3f2c8b6714a255c3e019c2
Vitor Gonçalves <vitorgondev@gmail.com>
2024-11-04 15:08:21 -0300

    bugfix

--- Script-js/areaUsuario.js
+++ Script-js/areaUsuario.js
@@ -32,7 +32,7 @@ toggle_opcoes_usuario.addEventListener("click", () => {
     seta_menu_opcoes_usuario.src = "Imagens/seta_direita.png";
   } else {
     conteudo_opcoes_usuario.style.display = "flex";
-    nav_opcoes_usuario.style.display = "block";
+    nav_opcoes_usuario.style.display = "flex";
     container_opcoes_usuario.style.width = "40%";
     toggle_opcoes_usuario.style.width = "18%";
     seta_menu_opcoes_usuario.src = "Imagens/seta_esquerda.png";
